a97809ddbb29fa75e19752e411cc180f52e10c75
Angel <sevinchoyazimboevna@gmail.com>
2026-08-07 06:13:51 +0500

    feat: build project standarts

--- apps/nestar-api/src/app.resolver.ts
+++ apps/nestar-api/src/app.resolver.ts
@@ -1,7 +1,7 @@
 import {Query, Resolver} from "@nestjs/graphql";
 
 @Resolver()
-export class AppResolver { //
+export class AppResolver { 
   @Query(() => String)
   public sayHello(): string {
     return 'GraphQL server api!';
---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,20 @@
+export enum Message {
+	SOMETHING_WENT_WRONG = 'Something went wrong!',
+	NO_DATA_FOUND = 'No data is found!',
+	CREATE_FAILED = 'Create is failed!',
+	UPDATE_FAILED = 'Update is failed!',
+	REMOVE_FAILED = 'Remove failed!',
+	BAD_REQUEST = 'Bad Request',
+
+	USED_NICK_PHONE = 'You are inserting already used nick or phone!',
+	TOKEN_CREATION_FAILED = 'Token creation error!',
+	NO_MEMBER_NICK = 'no member with that member nick!',
+	BLOCKED_USER = 'You have been blocked, contact Restaurnt!',
+	WRONG_PASSWORD = 'Wrong password entered, please try again!',
+	NOT_AUTHENTICATED = 'You are not authenticated, Please login first!',
+	TOKEN_NOT_EXIST = 'Bearer Token is not provided',
+	ONLY_SEPCIFIC_ROLES_ALLOWED = 'Allowed only for member with specific roles!',
+	NOT_ALLOWED_REQUEST = 'Not Allowed Request!',
+	PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg or png images!',
+	SELF_SUBSCRUPTION_DENIED = 'Self subscruption is denied!',
+}
