894d820f134fc4a0eb4052175c2040a131e1ecf2
Angel <sevinchoyazimboevna@gmail.com>
2026-08-07 05:52:46 +0500

    feat: intigration GraphQlModule and build first GRAPHQL query api

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config"
+import {GraphQLModule} from "@nestjs/graphql"
+import { ApolloDriver} from "@nestjs/apollo"
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [ConfigModule.forRoot()], //Load environment variables from .env file
+  imports: [
+    ConfigModule.forRoot(), // Load en variables from env file
+    GraphQLModule.forRoot({ // bu Graphql ni ishga tushuradi va schemani avtomatik yaratadi
+      driver: ApolloDriver, 
+      playground: true, 
+      uploads: false,// faylarni yuklashni ochiradi
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
