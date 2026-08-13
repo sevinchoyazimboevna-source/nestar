30a4eace5bb02ba14e08004275a3024c66d798fe
Angel <sevinchoyazimboevna@gmail.com>
2026-08-13 08:29:33 +0500

    feat: develop login business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import MemberSchema from './../../../../schemas/Member.model';
 import { Model } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
-import { MemberInput } from '../../libs/dto/member/member.input';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { MemberStatus } from '../../libs/enums/member.enum';
+import { Message } from '../../libs/enums/common.enum';
 
 //ASOSY BUSINESS MANTIQ shu yerda yoziladi
 
@@ -25,8 +27,24 @@ export class MemberService {
         }
     }
 
-    public async login(): Promise<string> {
-        return "login executed!";
+    public async login(input: LoginInput): Promise<Member> {
+        const {memberNick, memberPassword} = input;
+        const response: Member | null = await this.memberModel
+        .findOne({memberNick: memberNick})
+        .select('+memberPassword')
+        .exec();
+
+        if(!response || response.memberStatus === MemberStatus.DELETE) {
+            throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+        } else if (response.memberStatus === MemberStatus.BLOCK) {
+            throw new InternalServerErrorException(Message.BLOCKED_USER);
+        }
+
+        //TODO: COMPARE PASSWORD
+        const isMatch = memberPassword === response.memberPassword;
+        if(!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD)
+
+        return response;
     }
 
     public async updateMember(): Promise<string> {
