c43f0aad068759cec45c730c7ed364b1850a7408
Angel <sevinchoyazimboevna@gmail.com>
2026-08-13 05:30:15 +0500

    feat: create member schema model

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import MemberSchema from './../../../../schemas/Member.model';
+import { Model } from 'mongoose';
 
 //ASOSY BUSINESS MANTIQ shu yerda yoziladi
 
 @Injectable()
 export class MemberService {
+
+    constructor(@InjectModel("Member") private readonly memberModel: Model<null>) {}
 
     public async signup(): Promise<string> {
         return "signup executed!";
