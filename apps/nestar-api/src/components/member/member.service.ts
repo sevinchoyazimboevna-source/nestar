82b962117d1b0737ac75d968c8ed7ec502e42698
Angel <sevinchoyazimboevna@gmail.com>
2026-08-25 05:16:07 +0500

    feat: create get member graphQl api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Member } from '../../libs/dto/member/member';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { MemberStatus } from '../../libs/enums/member.enum';
 import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { T } from '../../libs/types/common';
 
 //ASOSY BUSINESS MANTIQ shu yerda yoziladi
 
@@ -74,8 +75,17 @@ export class MemberService {
         return result;
     }
 
-    public async getMember(): Promise<string> {
-        return "getMember executed!";
+    public async getMember(targetId: ObjectId): Promise<Member> {
+        const search: T = {
+            _id: targetId,
+            memberStatus: {
+                $in: [MemberStatus.ACTIVE, MemberStatus.BLOCK],
+            },
+        };
+        const targetMember = await this.memberModel
+        .findOne(search).exec();
+        if(!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        return targetMember;
     }
 
     public async getAllMembersByAdmin(): Promise<string> {
