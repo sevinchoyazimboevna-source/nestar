2a72b469072f17a2008a8c391a37e558ce97de29
Angel <sevinchoyazimboevna@gmail.com>
2026-08-28 05:10:17 +0500

    feat: develop updateProperty business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -10,6 +10,7 @@ import { Property } from '../../libs/dto/property/property';
 import { PropertyInput } from '../../libs/dto/property/property.input';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { PropertyUpdate } from '../../libs/dto/property/property.update';
 
 @Resolver()
 export class PropertyResolver {
@@ -38,4 +39,19 @@ export class PropertyResolver {
     const propertyId = shapeIntoMongoObjectId(input);
     return await this.propertyService.getProperty(memberId, propertyId);
    }
+
+
+   @Roles(MemberType.AGENT)
+   @UseGuards(RolesGuard)
+   @Mutation((returns) => Property)
+   public async updateProperty(
+   @Args('input') input: PropertyUpdate,
+   @AuthMember('_id') memberId: ObjectId,
+   ): Promise<Property> {
+   console.log('Mutation: updateProperty');
+   input._id = shapeIntoMongoObjectId(input._id);
+   return await this.propertyService.updateProperty(memberId, input);
+}
+
+
 }
