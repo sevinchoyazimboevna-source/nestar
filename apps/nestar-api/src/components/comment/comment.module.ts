097a553f518afcb2a840bafa4a3abbada8c1ec45
Angel <sevinchoyazimboevna@gmail.com>
2026-09-02 22:02:45 +0500

    feat: creating resolver and service for follow and like components

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { CommentResolver } from './comment.resolver';
 import { CommentService } from './comment.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import PropertySchema from '../../schemas/Property.model';
 import { AuthModule } from '../auth/auth.module';
 import { MemberModule } from '../member/member.module';
 import { ViewModule } from '../view/view.module';
-import CommentSchema from '../../schemas/Comment.model';
 import { PropertyModule } from '../property/property.module';
 import { BoardArticleModule } from '../board-article/board-article.module';
+import CommentSchema from '../../libs/schemas/Comment.model';
 
 @Module({
 	imports: [
--- apps/nestar-api/src/components/follow/follow.module.ts
+++ apps/nestar-api/src/components/follow/follow.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { FollowResolver } from './follow.resolver';
+import { FollowService } from './follow.service';
 
-@Module({})
+@Module({
+  providers: [FollowResolver, FollowService]
+})
 export class FollowModule {}
