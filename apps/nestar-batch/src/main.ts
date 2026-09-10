b3e8c82a966371c76cc14d9b6427ab141478bfb5
Angel <sevinchoyazimboevna@gmail.com>
2026-09-10 18:40:07 +0500

    feat: develop job schedule business logic

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,8 +1,11 @@
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 import { NestFactory } from '@nestjs/core';
-import { NestarBatchModule } from './nestar-batch.module';
+import { BatchModule } from './batch.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(process.env.PORT_BATCH ?? 3000);
+	const app = await NestFactory.create(BatchModule);
+	await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
